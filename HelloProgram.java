ee3e76d4738aecb24657903bbe4ac8122e293731
Jenny Tong <j.tong89@gmail.com>
2017-03-03 00:44:21 -0600

    changed the color and font of hello world

--- HelloProgram.java
+++ HelloProgram.java
@@ -1,8 +1,13 @@
 import acm.graphics.*;
 import acm.program.*;
 
+import java.awt.*;
+
 public class HelloProgram extends GraphicsProgram {
     public void run () {
-        add( new GLabel("hello world", 100, 75) );
+        GLabel label = new GLabel("hello world", 100, 75);
+        label.setFont("SansSerif-36");
+        label.setColor(Color.RED);
+        add(label);
     }
 }
