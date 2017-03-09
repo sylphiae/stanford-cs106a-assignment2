44cffb62120810188aebc9115c0542f819e02b96
Jenny Tong <j.tong89@gmail.com>
2017-03-09 15:11:32 -0600

    Solved the target problem and refactored

--- Target.java
+++ Target.java
@@ -8,32 +8,58 @@
 
 import acm.graphics.*;
 import acm.program.*;
+
 import java.awt.*;
 
-public class Target extends GraphicsProgram {	
-	public void run() {
-		int horizontalMidpoint = getWidth()/2;
-		int verticalMidpoint = getHeight()/2;
+public class Target extends GraphicsProgram {
 
-        GOval test = new GOval(horizontalMidpoint, verticalMidpoint, 1, 1);
-		GOval outerRedCircle = new GOval(horizontalMidpoint-72, verticalMidpoint-72, 144, 144);
-		outerRedCircle.setColor(Color.RED);
-		outerRedCircle.setFilled(true);
-		outerRedCircle.setFillColor(Color.RED);
-		add(outerRedCircle);
+    public void run() {
+        drawCircle(144, Color.RED);
+        drawCircle(97.5, Color.WHITE);
+        drawCircle(45, Color.RED);
+    }
 
-        GOval whiteCircle = new GOval (horizontalMidpoint-48.75, verticalMidpoint-48.75, 97.5, 97.5);
-        whiteCircle.setColor(Color.WHITE);
-        whiteCircle.setFilled(true);
-        whiteCircle.setFillColor(Color.WHITE);
-        add(whiteCircle);
 
-        GOval innerRedCircle = new GOval (horizontalMidpoint-22.5, verticalMidpoint-22.5, 45, 45);
-        innerRedCircle.setColor(Color.RED);
-        innerRedCircle.setFilled(true);
-        innerRedCircle.setFillColor(Color.RED);
-        add(innerRedCircle);
+    private void drawCircle(double diameter, Color fillColor) {
+        GOval circle = (buildCircle(diameter));
+        fillCircle(circle, fillColor);
+        add(circle);
+    }
 
-        add(test);
-	}
+
+    private void fillCircle(GOval circle, Color fillColor) {
+        circle.setColor(fillColor);
+        circle.setFilled(true);
+        circle.setFillColor(fillColor);
+    }
+
+
+    private GOval buildCircle(double diameter) {
+        double radius = diameter / 2;
+        int horizontalMidpoint = getWidth() / 2;
+        int verticalMidpoint = getHeight() / 2;
+        return new GOval(horizontalMidpoint - radius, verticalMidpoint - radius, diameter, diameter);
+    }
+
+
+    /**GOval outerRedCircle = new GOval(horizontalMidpoint - 72, verticalMidpoint - 72, 144, 144);
+     outerRedCircle.setColor(Color.RED);
+     outerRedCircle.setFilled(true);
+     outerRedCircle.setFillColor(Color.RED);
+     add(outerRedCircle);
+
+     GOval whiteCircle = new GOval(horizontalMidpoint - 48.75, verticalMidpoint - 48.75, 97.5, 97.5);
+     whiteCircle.setColor(Color.WHITE);
+     whiteCircle.setFilled(true);
+     whiteCircle.setFillColor(Color.WHITE);
+
+     add(whiteCircle);
+
+     GOval innerRedCircle = new GOval(horizontalMidpoint - 22.5, verticalMidpoint - 22.5, 45, 45);
+     innerRedCircle.setColor(Color.RED);
+     innerRedCircle.setFilled(true);
+     innerRedCircle.setFillColor(Color.RED);
+
+     add(innerRedCircle);
+     **/
 }
